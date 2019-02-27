89723d9fbfe3b00227955ea0262b80daddf4ff2e
faina7748 <arina_melissa_idayu@yahoo.com>
2019-02-27 11:54:59 +0800

    update

--- client.php
+++ client.php
@@ -9,12 +9,14 @@ $films = json_decode($result->getBody()->getContents());
 
 echo "<h3><img src='img/film.jpg' width='50' height='50'>My Favourite Film List</h3>";
 echo '<table border=1 cellpadding=0 cellspacing=0>';
-echo '<tr bgcolor="#ff99dd"><td>No</td><td>Title</title><td>Description</td>';
+echo '<tr bgcolor="#ff99dd"><td>No</td><td>Title</title><td>Description</td><td>Language</td><td>Show</td>';
 foreach ($films as $key => $film) {	
 	echo '<tr>';
 	echo '<td align="center">'.($key+1).'.</td>';
 	echo '<td>'.$film->title.'</td>';
 	echo '<td>'.$film->descr.'</td>';
+	echo '<td align="center">'.$film->lang.'</td>';
+	echo '<td align="center"><a href="img/smile.jpg" target=_blank><img src="img/movie.png" height="20" width="20"></a></td>';
 	echo '</tr>';
 }
 echo '</table>';
---
+++ vendor/frm_detail.php
@@ -0,0 +1,7 @@
+<h3>Film Detail</h3>
+<form action="film_detail.php">
+  Film ID:<br>
+  <input type="text" name="id">  
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
